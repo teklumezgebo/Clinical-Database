c43cac98bf742748195eefe54d0d4858c465ed1a
teklumezgebo <mezge002@umn.edu>
2023-06-08 22:04:17 -0500

    routes updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get "/patients" do
-    
+    patients = Patient.all 
+    patients.to_json
   end
   
   get "/patients/:id" do
