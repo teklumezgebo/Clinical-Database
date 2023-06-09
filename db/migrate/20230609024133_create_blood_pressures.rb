072f65e4b76265b86c3f6b28319171ed51ccd9d1
teklumezgebo <mezge002@umn.edu>
2023-06-08 21:45:54 -0500

    model files created

--- db/migrate/20230609024133_create_blood_pressures.rb
+++ db/migrate/20230609024133_create_blood_pressures.rb
@@ -1,4 +1,10 @@
 class CreateBloodPressures < ActiveRecord::Migration[6.1]
   def change
+    create_table :blood_pressures do |t|
+      t.integer :systolic
+      t.integer :diastolic
+      t.integer :patient_id
+      t.timestamps
+    end
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2023_06_09_024133) do
+
+  create_table "blood_pressures", force: :cascade do |t|
+    t.integer "systolic"
+    t.integer "diastolic"
+    t.integer "patient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "blood_sugars", force: :cascade do |t|
+    t.integer "blood_sugar"
+    t.integer "patient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "hypertension"
+    t.boolean "diabetes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
