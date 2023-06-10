605117abf22a8af4f630830cd9418920efbc82b8
teklumezgebo <mezge002@umn.edu>
2023-06-09 20:57:38 -0500

    requests made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,23 @@ class ApplicationController < Sinatra::Base
     patient.to_json
   end
 
+  delete '/patients/:id' do |t|
+    patient = Patient.find(params[:id])
+    patient.destroy
+    patient.to_json
+  end
+
+  post '/patients' do |t|
+    patient = Patient.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      hypertension: params[:hypertension],
+      diabetes: params[:diabetes]
+    )
+    patient.to_json
+  end
+
+  patch '/patients/:id' do |t|
+    
+  end
 end
