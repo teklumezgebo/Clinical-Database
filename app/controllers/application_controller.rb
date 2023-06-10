725e657dd9a7e23a04703f480ded8680f9071015
teklumezgebo <mezge002@umn.edu>
2023-06-09 21:07:40 -0500

    get requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,13 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  get "/patients" do
+  get '/patients' do
     patients = Patient.all 
     patients.to_json
   end
   
   get "/patients/:id" do
     patient = Patient.find(params[:id])
-    patient.to_json
-  end
-
-  delete '/patients/:id' do |t|
-    patient = Patient.find(params[:id])
-    patient.destroy
     patient.to_json
   end
 
@@ -37,5 +31,31 @@ class ApplicationController < Sinatra::Base
     )
     patient.to_json
   end
-  
+
+  delete '/patients/:id' do |t|
+    patient = Patient.find(params[:id])
+    patient.destroy
+    patient.to_json
+  end
+
+  get '/clinicians' do |t| 
+    clinicians = Clinicians.all
+    clinicians.to_json
+  end
+
+  get '/clinicians/:id' do |t|
+    clinician = Clinician.find(params[:id])
+    clinician.to_json
+  end
+
+  get '/clinics' do |t|
+    clinics = Clinic.all
+    clinics.to_json
+  end
+
+  get '/clinics/:id' do |t|
+    clinic = Clinic.find(params[:id])
+    clinic.to_json
+  end
+
 end
