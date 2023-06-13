f95e596e4ebcc236a227bbf54bfdb7dd45f83efe
teklumezgebo <mezge002@umn.edu>
2023-06-12 20:47:47 -0500

    post request created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get '/patients' do
-    patients = Patient.all 
+    patients = Patient.all
     patients.to_json
   end
   
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
     patient.to_json
   end
 
-  post '/patients' do |t|
+  post '/patients' do 
     patient = Patient.create(
       first_name: params[:first_name],
       last_name: params[:last_name],
@@ -21,7 +21,22 @@ class ApplicationController < Sinatra::Base
     patient.to_json
   end
 
-  patch '/patients/:id' do |t|
+  post '/bloodpressure' do 
+    patient = Patient.find_by(params(:first_name), params(:last_name))
+    patient_id = patient.id
+
+
+    bloodpressure = BloodPressure.create(
+      systolic: params(:systolic),
+      diastolic: params(:diastolic),
+      patient_id: patient_id
+    )
+
+    bloodpressure.to_json
+
+  end
+
+  patch '/patients/:id' do 
     patient = Patient.find(params[:id])
     patient.update(
       first_name: params[:first_name],
@@ -32,29 +47,46 @@ class ApplicationController < Sinatra::Base
     patient.to_json
   end
 
-  delete '/patients/:id' do |t|
+  delete '/patients/:id' do 
     patient = Patient.find(params[:id])
     patient.destroy
     patient.to_json
   end
 
-  get '/clinicians' do |t| 
-    clinicians = Clinicians.all
+  get '/clinicians' do  
+    clinicians = Clinician.all
     clinicians.to_json
   end
 
-  get '/clinicians/:id' do |t|
+  get '/clinicians/:id' do 
     clinician = Clinician.find(params[:id])
     clinician.to_json
   end
 
-  get '/clinics' do |t|
-    clinics = Clinic.all
+  get '/clinics' do 
+    clinics = Clinic.all,
     clinics.to_json
   end
 
-  get '/clinics/:id' do |t|
+  get '/clinics/:id' do 
     clinic = Clinic.find(params[:id])
+    clinic.to_json
+  end
+
+  post '/clinicians' do 
+    clinician = Clinician.create(
+      name: params[:name],
+      title: params[:title]
+    )
+    clinician.to_json
+  end
+
+  post '/clinics' do 
+    clinic = Clinic.create(
+      name: params[:name],
+      location: params[:location],
+      clinicians: params[:clinicans]
+    )
     clinic.to_json
   end
 
